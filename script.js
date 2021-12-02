776fa2fd76c1add1b17b8e833f75e7b377cddea6
zplummer <zplummer@halix.io>
2021-12-01 20:56:06 -0800

    Added rnd to css link

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+(function() {
+    var h, a, f;
+    a = document.getElementsByTagName('link');
+    for (h = 0; h < a.length; h++) {
+      f = a[h];
+      if (f.rel.toLowerCase().match(/stylesheet/) && f.href) {
+        var g = f.href.replace(/(&|\?)rnd=\d+/, '');
+        f.href = g + (g.match(/\?/) ? '&' : '?');
+        f.href += 'rnd=' + (new Date().valueOf());
+      }
+    } // for
+  })()
 var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
